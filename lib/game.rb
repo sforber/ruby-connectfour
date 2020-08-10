7966795adb3f020c2ba4adbce0fdfa54dde596f6
Scott Forber <scottforber@gmail.com>
2020-08-10 13:09:22 +0100

    Finished logic and tests

--- lib/game.rb
+++ lib/game.rb
@@ -1,12 +1,12 @@
 class Game
     attr_accessor :places
     def initialize
-        @places = [['','','','','','',''],
-            ['','','','','','',''],
-            ['','','','','','',''],
-            ['','','','','','',''],
-            ['','','','','','',''],
-            ['','','','','','','']]
+        @places = [[' ',' ',' ',' ',' ',' ',' '],
+        [' ',' ',' ',' ',' ',' ',' '],
+        [' ',' ',' ',' ',' ',' ',' '],
+        [' ',' ',' ',' ',' ',' ',' '],
+        [' ',' ',' ',' ',' ',' ',' '],
+        [' ',' ',' ',' ',' ',' ',' ']]
         @turn = 0
         @gameEnd = false
     end
@@ -22,23 +22,38 @@ class Game
             end
             placePiece(column, @turn)
             updateDisplay()
-            @turn += 1
+            if winCheck? == true
+                @gameEnd = true
+                puts "\nCongratulations Player #{@turn % 2 + 1}, you win!"
+            else
+                @turn += 1
+            end
+
         end
     end
 
     def updateDisplay
-        puts "\n Display Updated \n"
+        system 'clear'
+        board = ''
+        reversePlaces = [[],[],[],[],[],[]]
+        for row in (0..5)
+            reversePlaces[5 - row] = @places[row]
+        end
+        board += "\n1|2|3|4|5|6|7"
+        for row in (0..5)
+            board += "\n#{reversePlaces[row][0]}|#{reversePlaces[row][1]}|#{reversePlaces[row][2]}|#{reversePlaces[row][3]}|#{reversePlaces[row][4]}|#{reversePlaces[row][5]}|#{reversePlaces[row][6]} "
+        end
+        puts board + "\n\n\n"
     end
     
     def placePiece(column, turn)
         piece = whichPlayer?(turn)
-        for i in (0..6) do
-            if @places[i][column] == ''
+        for i in (0..5) do
+            if @places[i][column] == " "
                 @places[i][column] = piece
                 break
             end
         end
-        puts @places[i][0]
     end
 
     def whichPlayer?(turn)
@@ -50,16 +65,68 @@ class Game
     end
 
     def validPosition?(column)
+        valid = false
         if column >= 0 && column <= 6
-            for i in (0..6) do
-                if @places[i][column].empty?
+            for i in (0..5) do
+                if @places[i][column] == " "
+                    valid = true
+                    return valid
+                end
+            end
+        end
+        return valid
+    end
+
+    def winCheck?
+        if horizontalWin == true
+            return true 
+        elsif verticalWin == true
+            return true
+        elsif diagonalWin == true
+            return true        
+        end
+    end
+
+    def horizontalWin
+        for row in (0..5)
+            for column in (0..3)
+                if @places[row][column] == @places[row][column + 1] && @places[row][column] == @places[row][column + 2] && @places[row][column] == @places[row][column + 3] && @places[row][column] != " "
                     return true
                 end
             end
         end
-    end
+        return false
+    end 
 
-    def winCheck?
+    def verticalWin
+        for column in (0..6)
+            if @places[0][column] == @places[1][column] && @places[0][column] == @places[2][column] && @places[0][column] == @places[3][column] && @places[0][column] != " "
+                return true
+            end
+        end
+        return false
+    end 
 
+    def diagonalWin
+        for row in (0..2)
+            for column in (0..2)
+                if @places[row][column] == @places[row + 1][column + 1] && @places[row][column] == @places[row + 2][column + 2] && @places[row][column] == @places[row + 3][column + 3] && @places[row][column] != " "
+                    return true
+                end
+            end
+            for column in (3...4)
+                if @places[row][column] == @places[row][column + 1] && @places[row][column] == @places[row][column + 2] && @places[row][column] == @places[row][column + 3] && @places[row][column] != " "
+                    return true
+                elsif @places[row][column] == @places[row + 1][column - 1] && @places[row][column] == @places[row + 2][column - 2] && @places[row][column] == @places[row + 3][column - 3] && @places[row][column] != " "
+                    return true 
+                end
+            end
+            for column in (4..6)
+                if @places[row][column] == @places[row + 1][column - 1] && @places[row][column] == @places[row + 2][column - 2] && @places[row][column] == @places[row + 3][column - 3] && @places[row][column] != " "
+                    return true
+                end 
+            end
+        end
+        return false
     end
 end
--- spec/connectfour_spec.rb
+++ spec/connectfour_spec.rb
@@ -5,7 +5,7 @@ describe "Game" do
         it "updates @places with the position of the players piece" do
             game = Game.new
             game.placePiece(0,0)
-            expect(game.places[6][0]).to eql("X")
+            expect(game.places[0][0]).to eql("X")
         end
     end
 
@@ -17,14 +17,72 @@ describe "Game" do
 
         it "returns true if column entered has at least 1 empty space" do
             game = Game.new
-            game.places[4][0], game.places[5][0], game.places[6][0] = 'X', 'X', 'X'
             expect(game.validPosition?(0)).to eql(true)
         end
 
-        it "returns false if column entered has at no empty spaces" do
+        it "returns false if column entered has no empty spaces" do
             game = Game.new
-            game.places[0][0], game.places[1][0], game.places[2][0], game.places[3][0], game.places[4][0], game.places[5][0], game.places[6][0] = 'X', 'X', 'X', 'X', 'X', 'X', 'X'
+            game.places = [['X',' ',' ',' ',' ',' ',' '],
+            ['X',' ',' ',' ',' ',' ',' '],
+            ['X',' ',' ',' ',' ',' ',' '],
+            ['X',' ',' ',' ',' ',' ',' '],
+            ['X',' ',' ',' ',' ',' ',' '],
+            ['X',' ',' ',' ',' ',' ',' ']]
             expect(game.validPosition?(0)).to eql(false)
+        end
+    end
+
+    describe "#horizontalWin" do
+        it "returns true if row contains four of the same symbols in a row" do
+            game = Game.new
+            game.places[0] = ['X','X','X','X',' ',' ',' ']
+            expect(game.horizontalWin).to eql(true)
+        end
+
+        it "returns false if row does not contain four of the same symbols in a row" do
+            game = Game.new
+            game.places[0] = ['X','X','X','O',' ',' ',' ']
+            expect(game.horizontalWin).to eql(false)
+        end
+    end
+
+    describe "#verticalWin" do
+        it "returns true if column contains four of the same symbols in a row" do
+            game = Game.new
+            game.places[0] = ['X',' ',' ',' ',' ',' ',' ']
+            game.places[1] = ['X',' ',' ',' ',' ',' ',' ']
+            game.places[2] = ['X',' ',' ',' ',' ',' ',' ']
+            game.places[3] = ['X',' ',' ',' ',' ',' ',' ']
+            expect(game.verticalWin).to eql(true)
+        end
+
+        it "returns false if column does not contain four of the same symbols in a row" do
+            game = Game.new
+            game.places[0] = ['X',' ',' ',' ',' ',' ',' ']
+            game.places[1] = ['X',' ',' ',' ',' ',' ',' ']
+            game.places[2] = ['X',' ',' ',' ',' ',' ',' ']
+            game.places[3] = ['O',' ',' ',' ',' ',' ',' ']
+            expect(game.verticalWin).to eql(false)
+        end
+    end
+
+    describe "#diagonalWin" do
+        it "returns true if there is a diagonal four of the same symbols" do
+            game = Game.new
+            game.places[0] = ['X',' ',' ',' ',' ',' ',' ']
+            game.places[1] = [' ','X',' ',' ',' ',' ',' ']
+            game.places[2] = [' ',' ','X',' ',' ',' ',' ']
+            game.places[3] = [' ',' ',' ','X',' ',' ',' ']
+            expect(game.diagonalWin).to eql(true)
+        end
+
+        it "returns false if there is a diagonal four of the same symbols" do
+            game = Game.new
+            game.places[0] = ['X',' ',' ',' ',' ',' ',' ']
+            game.places[1] = [' ','O',' ',' ',' ',' ',' ']
+            game.places[2] = [' ',' ','X',' ',' ',' ',' ']
+            game.places[3] = [' ',' ',' ','X',' ',' ',' ']
+            expect(game.diagonalWin).to eql(false)
         end
     end
 end
